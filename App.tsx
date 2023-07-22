75ff1548fa1265a113e0800d44837a0fe432273d
Icaro Apolo <apoloraci@gmail.com>
2023-07-22 14:24:04 -0300

    feat(navigation): add navigate and go back for auth routes, and apply defaultSorce img

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
-import { View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { Loading } from '@components/Loading';
+import { Routes } from './src/routes';
 import { THEME } from './src/theme';
-import { SignIn } from '@screens/SignIn';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -16,7 +16,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ NativeBaseProvider>
   );
 }
