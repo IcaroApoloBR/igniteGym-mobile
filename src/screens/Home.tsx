29728b1d7a7fb01bd4037aeef8454216b39e5f69
Icaro Apolo <apoloraci@gmail.com>
2023-07-22 19:08:45 -0300

    feat(component): group from workout day

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,32 @@
+import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
-import { VStack } from 'native-base';
+import { HStack, VStack, FlatList } from 'native-base';
+import { useState } from 'react';
 
 export function Home() {
+    const [groups, setGroups] = useState(['Costas', 'Peito', 'Perna', 'Ombro', 'Braço']);
+    const [groupSelected, setGroupSelected] = useState('costa');
+
     return (
         <VStack flex={1}>
             <HomeHeader />
+
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <Group
+                        name={item}
+                        isActive={groupSelected === item}
+                        onPress={() => setGroupSelected(item)}
+                    />
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                _contentContainerStyle={{ px: 8 }}
+                my={10}
+                maxHeight={10}
+            />
         </VStack>
     );
 }
