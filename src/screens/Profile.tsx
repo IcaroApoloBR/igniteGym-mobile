d442d16503859d2650aa25c8e58118a980e63529
Icaro Apolo <apoloraci@gmail.com>
2023-07-24 20:58:50 -0300

    feat(screen): profile with img and inputs for change password

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
-import { Center, ScrollView, VStack, Skeleton } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading } from 'native-base';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 
 const PHOTO_SIZE = 33;
 
@@ -11,7 +14,8 @@ export function Profile() {
     return (
         <VStack flex={1}>
             <ScreenHeader title="Perfil" />
-            <ScrollView>
+
+            <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
                 <Center mt={6} px={10}>
                     {
                         photoIsLoading ?
@@ -29,6 +33,50 @@ export function Profile() {
                                 size={PHOTO_SIZE}
                             />
                     }
+
+                    <TouchableOpacity>
+                        <Text color="yellow.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
+                            Alterar foto
+                        </Text>
+                    </TouchableOpacity>
+
+                    <Input
+                        bg="gray.600"
+                        placeholder="Nome"
+                    />
+
+                    <Input
+                        bg="gray.600"
+                        placeholder="E-mail"
+                        isDisabled
+                    />
+
+                    <Heading color="gray.200" fontSize="md" mb={2} mt={12} alignSelf="flex-start">
+                        Alterar senha
+                    </Heading>
+
+                    <Input
+                        bg="gray.600"
+                        placeholder="Senha antiga"
+                        secureTextEntry
+                    />
+
+                    <Input
+                        bg="gray.600"
+                        placeholder="Nova senha"
+                        secureTextEntry
+                    />
+
+                    <Input
+                        bg="gray.600"
+                        placeholder="Confirme a nova senha"
+                        secureTextEntry
+                    />
+
+                    <Button
+                        title="Atualizar"
+                        mt={4}
+                    />
                 </Center>
             </ScrollView>
         </VStack>
