89168cc3171361af0678818eaa1de5e7f288b3fd
Icaro Apolo <apoloraci@gmail.com>
2023-07-30 14:00:26 -0300

    feat(errors): use axios and app errors for show message to user after actions

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,12 +1,16 @@
 import { useNavigation } from '@react-navigation/native';
 import { useForm, Controller } from 'react-hook-form';
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from 'native-base';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import axios from 'axios';
+import { api } from '@services/api';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 import BackgroundImg from '@assets/background.png';
 import LogoSvg from '@assets/logo.svg';
+import { Alert } from 'react-native';
+import { AppError } from '@utils/AppError';
 
 type FormDataProps = {
     name: string;
@@ -26,6 +30,8 @@ export function SignUp() {
 
     const navigation = useNavigation();
 
+    const toast = useToast();
+
     const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
         resolver: yupResolver(signUpSchema)
     });
@@ -34,8 +40,22 @@ export function SignUp() {
         navigation.goBack();
     }
 
-    function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
-        console.log({ name, email, password, password_confirm });
+    async function handleSignUp({ name, email, password }: FormDataProps) {
+        try {
+            const response = await api.post('/users', { name, email, password });
+            console.log(response.data);
+
+        } catch (error) {
+            const isAppError = error instanceof AppError;
+            const title = isAppError ? error.message : 'Não foi possível criar conta. Tente novamente mais tarde.'
+
+            toast.show({
+                title,
+                placement: 'top',
+                bgColor: 'red.500'
+            });
+        }
+
     }
 
     return (
