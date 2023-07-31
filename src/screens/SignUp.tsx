637b1282ca310331c627b90e655b7bb8de5798e5
Icaro Apolo <apoloraci@gmail.com>
2023-07-31 15:22:23 -0300

    perf(sign up and sign in): after create account, user is login

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { useForm, Controller } from 'react-hook-form';
 import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from 'native-base';
@@ -9,8 +10,8 @@ import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 import BackgroundImg from '@assets/background.png';
 import LogoSvg from '@assets/logo.svg';
-import { Alert } from 'react-native';
 import { AppError } from '@utils/AppError';
+import { useAuth } from '@hooks/useAuth';
 
 type FormDataProps = {
     name: string;
@@ -27,10 +28,13 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp() {
+    const [isLoading, setIsLoading] = useState(false);
 
     const navigation = useNavigation();
 
     const toast = useToast();
+
+    const { signIn } = useAuth();
 
     const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
         resolver: yupResolver(signUpSchema)
@@ -42,10 +46,14 @@ export function SignUp() {
 
     async function handleSignUp({ name, email, password }: FormDataProps) {
         try {
-            const response = await api.post('/users', { name, email, password });
-            console.log(response.data);
+            setIsLoading(true)
+
+            await api.post('/users', { name, email, password });
+            await signIn(email, password);
 
         } catch (error) {
+            setIsLoading(false)
+
             const isAppError = error instanceof AppError;
             const title = isAppError ? error.message : 'Não foi possível criar conta. Tente novamente mais tarde.'
 
@@ -55,7 +63,6 @@ export function SignUp() {
                 bgColor: 'red.500'
             });
         }
-
     }
 
     return (
@@ -143,6 +150,7 @@ export function SignUp() {
                     <Button
                         title="Criar e acessar"
                         onPress={handleSubmit(handleSignUp)}
+                        isLoading={isLoading}
                     />
                 </Center>
 
