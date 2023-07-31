200c85d2e022f16ba20ab7fa1a70f6e1f9c890b8
Icaro Apolo <apoloraci@gmail.com>
2023-07-31 14:49:11 -0300

    feat(async storage): save async storage user logged, use in auth context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,12 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
 import { UserDTO } from '@dtos/UserDTO';
 import { api } from '@services/api';
+import { storageUserSave, storageUserGet } from '@storage/storageUser';
 
 export type AuthContextDataProps = {
     user: UserDTO;
     signIn: (email: string, password: string) => Promise<void>;
+    isLoadingUserStorageData: boolean;
 }
 
 type AuthContextProviderProps = {
@@ -17,6 +19,7 @@ export const AuthContext = createContext<AuthContextDataProps>(
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
     const [user, setUser] = useState<UserDTO>({} as UserDTO);
+    const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true)
 
     async function signIn(email: string, password: string) {
         try {
@@ -24,16 +27,36 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
             if (data.user) {
                 setUser(data.user);
+                storageUserSave(data.user);
             }
         } catch (error) {
             throw error;
         }
     }
 
+    async function loadUserData() {
+        try {
+            const userLogged = await storageUserGet();
+
+            if (userLogged) {
+                setUser(userLogged);
+            }
+        } catch (error) {
+            throw error;
+
+        } finally {
+            setIsLoadingUserStorageData(false);
+        }
+    }
+
+    useEffect(() => {
+        loadUserData();
+    }, []);
+
     return (
-        <AuthContext.Provider value= {{ user, signIn }
-}>
-    { children }
-    < /AuthContext.Provider>
-      );
+        <AuthContext.Provider value={{ user, signIn, isLoadingUserStorageData }
+        }>
+            {children}
+            < /AuthContext.Provider>
+            );
 }
