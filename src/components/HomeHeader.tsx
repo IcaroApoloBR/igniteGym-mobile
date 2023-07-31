1a578f63ddcd643bcde7e491e72680303bb1ca00
Icaro Apolo <apoloraci@gmail.com>
2023-07-31 15:12:43 -0300

    feat(signOut): add to context

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,12 +2,16 @@ import { HStack, VStack, Text, Heading, Icon } from 'native-base';
 import { MaterialIcons } from '@expo/vector-icons';
 import { UserPhoto } from './UserPhoto';
 import { TouchableOpacity } from 'react-native';
+import { useAuth } from '@hooks/useAuth';
+import defaultUserPhotoImg from '@assets/userPhotoDefault.png';
 
 export function HomeHeader() {
+    const { user, signOut } = useAuth();
+
     return (
         <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
             <UserPhoto
-                source={{ uri: 'https://github.com/IcaroApoloBR.png' }}
+                source={user.avatar ? { uri: user.avatar } : defaultUserPhotoImg}
                 alt="Imagem do usuário"
                 size={16}
                 mr={4}
@@ -19,11 +23,11 @@ export function HomeHeader() {
                 </Text>
 
                 <Heading color="gray.100" fontSize="md" fontFamily="heading">
-                    Ícaro Apolo
+                    {user.name}
                 </Heading>
             </VStack>
 
-            <TouchableOpacity>
+            <TouchableOpacity onPress={signOut}>
                 <Icon
                     as={MaterialIcons}
                     name="logout"
