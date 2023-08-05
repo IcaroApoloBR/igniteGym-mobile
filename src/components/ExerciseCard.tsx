f13e22cca5f00098115c5bf6869566053379784b
Icaro Apolo <apoloraci@gmail.com>
2023-08-05 16:11:15 -0300

    feat(group and exercises): fetch from api for dynamic data

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -1,32 +1,35 @@
 import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
 import { HStack, Heading, Image, Text, VStack, Icon } from 'native-base';
 import { Entypo } from '@expo/vector-icons';
+import { ExerciseDTO } from '@dtos/ExerciseDTO';
+import { api } from '@services/api';
 
 type Props = TouchableOpacityProps & {
-
+    data: ExerciseDTO;
 };
 
-export function ExerciseCard({ ...rest }: Props) {
+export function ExerciseCard({ data, ...rest }: Props) {
     return (
         <TouchableOpacity {...rest}>
             <HStack bg="gray.500" alignItems="center" p={2} pr={4} rounded="md" mb={3}>
                 <Image
-                    source={{ uri: 'https://blog.gsuplementos.com.br/wp-content/uploads/2021/04/iStock-1246046696.jpg' }}
+                    source={{ uri: `${api.defaults.baseURL}/exercise/thumb/${data.thumb}` }}
                     alt="Imagem do exercício"
                     w={16}
                     h={16}
                     rounded="md"
                     mr={4}
                     resizeMode="cover"
+                // resizeMode="center"
                 />
 
                 <VStack flex={1}>
                     <Heading fontSize="lg" color="white" fontFamily="heading">
-                        Supino inclinado
+                        {data.name}
                     </Heading>
 
                     <Text fontSize="sm" color="gray.200" mt={1} numberOfLines={2}>
-                        3 séries x 15 repetições
+                        {data.series} séries x {data.repetitions} repetições
                     </Text>
                 </VStack>
 
